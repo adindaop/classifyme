057f6ba974bc576adfb094811676c6561ffdf438
Adinda <adindaoctadia@gmail.com>
2017-03-28 19:17:21 +0700

    buat form ok

--- classifyme/views.py
+++ classifyme/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .forms import BukuForm
 from .models import Buku
 from django.shortcuts import redirect
@@ -10,5 +10,16 @@ def home(request):
     return render(request, 'classifyme/home.html', {})
 
 def input(request):
-    form = BukuForm()
+    if request.method == "POST":
+        form = BukuForm(request.POST)
+        if form.is_valid():
+            buku = form.save(commit=False)
+            buku.save()
+            return redirect('textmining', id=buku.id)
+    else:
+        form = BukuForm()
     return render(request, 'classifyme/input.html', {'form': form})
+
+def textmining(request):
+    buku = Buku.objects.get(id=buku_id)
+    return render(request, 'classifyme/textmining.html', {})
