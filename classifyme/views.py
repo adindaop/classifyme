c681e9336d0d335a742d7dc4fdc437da3341f9c3
Adinda <adindaoctadia@gmail.com>
2017-03-31 12:00:28 +0700

    proses text mining ok

--- classifyme/views.py
+++ classifyme/views.py
@@ -4,7 +4,8 @@ from .forms import BukuForm
 from .models import Buku
 from django.shortcuts import redirect
 from nltk.tokenize import word_tokenize
-
+from nltk.corpus import stopwords
+from nltk.util import ngrams
 
 def admin(request):
     return render(request, admin.site.urls, {})
@@ -22,7 +23,7 @@ def textmining(request, buku_id):
     buku = get_object_or_404(Buku, id=buku_id)
     context = {}
     selected_buku = Buku.objects.get(id=int(buku_id))
-    context['judul'] = selected_buku
+    context['buku'] = selected_buku
 
     print(selected_buku.ulasan)
 
@@ -38,10 +39,35 @@ def klasifikasi(request, buku_id):
     context = {}
     selected_buku = Buku.objects.get(id=int(buku_id))
     context['judul'] = selected_buku
+
+    ulasan = ''
+    with open('media/{}'.format(selected_buku.ulasan)) as csvfile:
+        csvreader = csv.DictReader(csvfile, delimiter=';', quotechar='"')
+        for row in csvreader:
+            ulasan += row['Review']
+
+    case_folding = ulasan.lower()
+    tokenized_words = word_tokenize(case_folding)
+
+    stop_words = set(stopwords.words("bahasa"))
+    filtered_sentence = []
+    for w in tokenized_words:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+
+    word_list = set(stopwords.words("wordlist"))
+    filtered_word = []
+    for a in tokenized_words:
+        if a in word_list:
+            filtered_word.append(a)
+
+    bigrams = list(ngrams(filtered_word,2))
+    context['bigrams'] = bigrams
+
     return render(request, 'classifyme/klasifikasi.html', context)
 
 def hasil(request, buku_id):
     context = {}
     selected_buku = Buku.objects.get(id=int(buku_id))
-    context['judul'] = selected_buku
+    context['buku'] = selected_buku
     return render(request, 'classifyme/hasil.html', context)
--- klasifikasiteks/urls.py
+++ klasifikasiteks/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('classifyme.urls')),
+    url(r'', include('classifyme.urls', namespace='classifyme')),
 ]
 
 if settings.DEBUG:
